a064e8cf5d2ec376b077de5918d0278a2103e050
Dan Yael Sajaropulos Verdugo <danyael031@gmail.com>
2020-04-22 02:58:28 -0600

    Adding random alphanumeric lines function

--- rln.sh
+++ rln.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+
+randomline(){
+strln=""
+while (( ${#strln} < 15 )); do
+	strln=${strln}$(head -c 128 /dev/urandom | tr -dc 'a-zA-Z0-9' )
+done
+echo ${strln:0:15}
+}
+
+for (( i = 0 ; i < 100 ; i++ )) ; do
+	randomline
+done
+
+	
