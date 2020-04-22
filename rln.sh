d5fd9b624b21500c652812b60f6c7d238bec6fa4
Dan Yael Sajaropulos Verdugo <danyael031@gmail.com>
2020-04-22 07:02:35 -0600

    comments about size and iteration

--- rln.sh
+++ rln.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# The function to generate each line of random characters its designed to 
+# take 128 bytes form /dev/urandom and filter it to only get alhpanumeric
+# ascii characters. If we don't get the 15 alphanumeric bytes in the first iteration,
+# we will get more bytes in te next iteration to accomplish the number of
+# characters per line. This solution can be more eficient than those that 
+# use a delimiter character from a random output.
+
 randomline(){
 strln=""
 while (( ${#strln} < 15 )); do
@@ -8,8 +15,39 @@ done
 echo ${strln:0:15}
 }
 
-for (( i = 0 ; i < 100 ; i++ )) ; do
-	randomline
+# To control the file size we can write each line to the destination file 
+# with output redirection:
+# $ randomline >> output
+# and verify the size of the file in bytes with the commands du:
+# $ size=$(du -b output | cut -f1 )
+# or using ls
+# $ size=$(ls -l output | cut -d ' ' -f5 )
+# But we dont need to verify the size of the file each line.
+#
+# If we have an static number of characters per line, 15, plus the Line Feed
+# character , 16, we can simply calculate the number of lines we need to reach 
+# 1MB of size.
+#
+# 1MB = 1,048,576 Bytes
+# 1,048,576 Bytes / 16 Bytes = 65,536 iterations
+#
+# we only need to iterate the function 65,356 times.
+# This method is more efficient becasue it only needs to
+# store the file in ram before write it and doesn't take time to read
+# from disk.
+
+randfile=""
+for (( i = 0 ; i < 65536 ; i++ )) ; do
+	randfile=${randfile}$(randomline)
 done
+
+
+
+# To 
+
+
+#for (( i = 0 ; i < 100 ; i++ )) ; do
+#	randomline
+#done
 
 	
